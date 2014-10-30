2c3d0508807c2794479c75690a8eb7f88bee716d
Thomas Rahn <thomas@rahn.ca>
2014-10-30 17:23:02 -0400

    Player movement and starting clusters

---
+++ Assets/Code/Scripts/player/Player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float sensitivityX = 15.0f;
+	public float sensitivityY = 15.0f;
+	public Quaternion originalRotation;
+
+	public float rotationX;
+	public float rotationY;
+
+	public GameObject bullet;
+	void Start () {
+		originalRotation = this.transform.rotation;
+		rotationX = 0;
+		rotationY = 0;
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate (Vector3.forward * 5.0f * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate (-Vector3.forward * 5.0f * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate (-Vector3.right * 5.0f * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate (Vector3.right * 5.0f * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.LeftShift)){
+			transform.Translate (-Vector3.up * 5.0f * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.Space)){
+			transform.Translate (Vector3.up * 5.0f * Time.deltaTime);
+		}
+
+
+		rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+		Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+		Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, -Vector3.right);
+		
+		transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+		
+		if(Input.GetKey(KeyCode.LeftControl) || Input.GetButton("Fire1")){
+			Shoot ();
+		}
+
+	}
+
+
+	public void Shoot(){
+		if(bullet != null){
+			GameObject shoot = Instantiate(bullet,this.transform.position, Quaternion.identity) as GameObject;
+			shoot.rigidbody.velocity = transform.forward * 10.0f;
+			Destroy(shoot, 1.5f);
+		}
+	}
+}
---
+++ Assets/Code/Util/Wrap.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wrap : MonoBehaviour {
+	public float limit = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = this.transform.position;
+		Vector3 newPosition = position;
+		if(position.x > limit || position.x < -limit){
+			newPosition.x = -position.x;
+		}
+		if(position.y > limit || position.y < -limit){
+			newPosition.y = -position.y;
+		}
+		if(position.z > limit || position.z < -limit){
+			newPosition.z = -position.z;
+		}
+		this.transform.position = newPosition;
+	}
+}
